3f525f487069f13b8d3522da349fe332f345932f
GenPike <olexis552@gmail.com>
2015-08-06 17:17:56 +0300

    add entities

---
+++ SmartExamples/SmartExamples/Models/BankRates.cs
@@ -0,0 +1,26 @@
+﻿namespace SmartExamples.Models
+{
+    public class LowBankRate : IRate
+    {
+        public double GetInterestRate()
+        {
+            return 1.1;
+        }
+    }
+
+    public class MiddleBankRate : IRate
+    {
+        public double GetInterestRate()
+        {
+            return 1.2;
+        }
+    }
+
+    public class BigBankRate : IRate
+    {
+        public double GetInterestRate()
+        {
+            return 1.3;
+        }
+    }
+}
