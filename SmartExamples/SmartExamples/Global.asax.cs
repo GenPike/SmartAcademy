2178fba699c4c7a9eac48cb6644b163b37a0601d
GenPike <olexis552@gmail.com>
2015-08-06 17:18:55 +0300

    configure unity

--- SmartExamples/SmartExamples/Global.asax.cs
+++ SmartExamples/SmartExamples/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SmartExamples.App_Start;
 
 namespace SmartExamples
 {
@@ -13,6 +14,7 @@ namespace SmartExamples
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            UnityConfig.RegisterTypes(UnityConfig.GetConfiguredContainer());
         }
     }
 }
