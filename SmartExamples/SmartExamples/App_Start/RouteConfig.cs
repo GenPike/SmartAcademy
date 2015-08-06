b489a69c7f051279cadee874fa1bfb72d38de801
GenPike <olexis552@gmail.com>
2015-08-06 17:19:24 +0300

    change start controller

--- SmartExamples/SmartExamples/App_Start/RouteConfig.cs
+++ SmartExamples/SmartExamples/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SmartExamples
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Rate", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
