8015ef748086c0c6857ed0a81d03a5b4934e4719
GenPike <olexis552@gmail.com>
2015-08-06 17:18:11 +0300

    add controller

--- SmartExamples/SmartExamples/Controllers/RateController.cs
+++ SmartExamples/SmartExamples/Controllers/RateController.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using SmartExamples.Models;
 
 namespace SmartExamples.Controllers
 {
     public class RateController : Controller
     {
-        // GET: Rate
-        public ActionResult Index()
+        public readonly IRate Rater = null;
+
+        public RateController(IRate tmpRater)
         {
-            return View();
+            Rater = tmpRater;
+        }
+        // GET: Rate
+        public string Index()
+        {
+            var currentRate = Rater.GetInterestRate();
+            var result = String.Format("Current bank interest rate is {0}", currentRate);
+            return result;
         }
     }
 }
